e40cb0bd5caa3666715462e70ece7a20ddc86794
yim666 <yimengruhun@gmail.com>
2023-01-28 14:07:47 +0800

    22

---
+++ producer/src/main/java/com/example/producer/config/RabbitMqConfiguration2.java
@@ -0,0 +1,33 @@
+package com.example.producer.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfiguration2 {
+
+    /*定义队列名称*/
+    public static final String QUEUE_NAME = "boot_queue2";
+    /*交换机*/
+//    @Bean("bootExchange")
+//    public Exchange bootExchange(){
+//        return ExchangeBuilder.topicExchange(EXCHANGE_NAME).durable(true).build();
+//    }
+    /* 2.Queue 队列*/
+    @Bean("bootQueue2")
+    public Queue bootQueue(){
+        return QueueBuilder.durable(QUEUE_NAME).build();
+    }
+    /*队列和交换机绑定关系*/
+    /*
+     * 知道哪个队列
+     * 知道哪个交换机
+     * routing key
+     * */
+//    @Bean
+//    public Binding getBinding(@Qualifier("bootQueue2") Queue queue, @Qualifier("bootExchange") Exchange exchange){
+//        return BindingBuilder.bind(queue).to(exchange).with("boot.#").noargs();
+//    }
+}
--- producer/src/test/java/com/example/producer/ProducerApplicationTests.java
+++ producer/src/test/java/com/example/producer/ProducerApplicationTests.java
@@ -1,12 +1,22 @@
 package com.example.producer;
 
+import ch.qos.logback.core.joran.util.beans.BeanUtil;
 import com.example.producer.config.RabbitMqConfiguration;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.ApplicationContextTestUtils;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextLoader;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -15,13 +25,39 @@ class ProducerApplicationTests {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    @Autowired
+    private RabbitAdmin rabbitAdmin;
+
     @Test
-    public void testSend(){
+    public void testSend() {
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("aa");
+        strings.add("bb");
+        strings.add("cc");
         //生产者发送消息
-        rabbitTemplate.convertAndSend(RabbitMqConfiguration.EXCHANGE_NAME,"boot.h","boot mq hello~~~");
-    }
-    @Test
-    void contextLoads() {
+        rabbitTemplate.convertAndSend(RabbitMqConfiguration.EXCHANGE_NAME, "boot.h", strings);
+        rabbitTemplate.convertAndSend("boot_topic_exchange", "boot.h", "boot topic hello~~~");
     }
 
+    @Test
+    void createExchange() {
+        //createExchange
+        Exchange adminExchange = ExchangeBuilder.directExchange("adminExchange").durable(true).build();
+        System.out.println(adminExchange);
+        rabbitAdmin.declareExchange(adminExchange);
+    }
+
+    @Test
+    void createQueue() {
+        //* The queue is durable, non-exclusive and non auto-delete.
+        rabbitAdmin.declareQueue(new Queue("amqpQueue21"));
+        System.out.println("success");
+    }
+
+    @Test
+    void createBinding(){
+        rabbitAdmin.declareBinding(new Binding("amqpQueue21", Binding.DestinationType.QUEUE,"adminExchange","amqp.#",null));
+        System.out.println("success");
+
+    }
 }
